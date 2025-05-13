9697a120e820f23f782512d55f2b405ac3444727
bulutluoz <abbulutluoz@gmail.com>
2025-05-13 21:36:33 +0200

    6.gun sonu

--- src/test/java/tests/day06_jUnitFramework/C06_AfterEachMantikliMi.java
+++ src/test/java/tests/day06_jUnitFramework/C06_AfterEachMantikliMi.java
@@ -1,5 +1,7 @@
 package tests.day06_jUnitFramework;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,12 +14,53 @@ import java.time.Duration;
 
 public class C06_AfterEachMantikliMi {
 
+    /*
+        Biz @Before... ve @After... method'lari kullanmadan da
+        @Test method'lari olusturabiliriz
+
+        ANCAKKKKKKK
+        Eger before ve after method'lari kullanmazsak 2 negatif durum olusur
+        1- birden fazla test method'umuz varsa
+           her bir test method'u icin bastaki ayarlamalari
+           ve sondaki kapatmayi
+           tekrar tekrar yazmamiz gerekir
+           (Kodlamada en istenmeyen sey kod tekraridir)
+
+         2- test methodunun icinde
+            en sonda driver.quit() kullanirsak
+            test method'u icinde kodlarda hata oldugunda
+            quit() calismaz
+            YAANNNNIIII failed olan her test method'u icin browser acik kalir
+
+         Bu iki sebepten dolayi
+         ister tekbir test() olsun
+         isterse cok test() olsun
+         Before ve After notasyonlarini KULLANMALIYIZ
+
+         Before ve After notasyonlarini KULLANMANIN
+         bir diger artisi da
+         @Test method'larinda SADECE testlere odaklanmamiz
+         driver olusturmak, ayarlamalar yapmak veya driver'i kapatmak gibi
+         detaylari Test()'nun disina atmis oluyoruz
+
+     */
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void teardown(){
+        driver.quit();
+    }
 
     @Test
     public void testotomasyonuTest(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         // Testotomasyonu ana sayfaya gidin
         driver.get("https://www.testotomasyonu.com");
@@ -40,10 +83,8 @@ public class C06_AfterEachMantikliMi {
         } else {
             System.out.println("Arama testi PASSED");
         }
-
-
         // sayfayi kapatin
-        driver.quit();
+
     }
 
 
