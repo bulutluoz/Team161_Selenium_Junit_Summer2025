4d57e153f3f277acb3af960f2c9748fa58955a5f
bulutluoz <abbulutluoz@gmail.com>
2025-05-14 21:38:10 +0200

    7.gun sonu

---
+++ src/test/java/tests/day07_assertions/C05_RadioButton.java
@@ -0,0 +1,12 @@
+package tests.day07_assertions;
+
+public class C05_RadioButton {
+
+    // 	a. Verilen web sayfasına gidin.
+    //	      https://testotomasyonu.com/form
+    //	b. Cinsiyet Radio button elementlerini locate edin
+    //	c. Iki farkli test method’u oluşturup yazidan veya direk buton’dan size uygun olani secin
+    //	d. Sectiginiz radio button’un seçili, ötekilerin seçili olmadigini test edin
+
+
+}
