345d9c6d661225946eee1765f077ec3b63ef7909
bulutluoz <abbulutluoz@gmail.com>
2025-05-14 20:46:07 +0200

    7.gun sonu

---
+++ src/test/java/tests/day07_assertions/C03_Assertions.java
@@ -0,0 +1,13 @@
+package tests.day07_assertions;
+
+public class C03_Assertions {
+
+    // https://www.bestbuy.com/ Adresine gidin
+    // farkli test method’lari olusturarak asagidaki testleri yapin
+    //	○ Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
+    //	○ titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
+    //	○ logoTest => BestBuy logosunun görüntülendigini test edin
+    //	○ FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
+
+
+}
