22c7d8e369849f0153637828ba9f6be8dae8cdf9
bulutluoz <abbulutluoz@gmail.com>
2025-05-15 19:48:01 +0200

    8.gun sonu

---
+++ src/test/java/tests/day08_testbase_dropdown/C02_DropdownMenu.java
@@ -0,0 +1,28 @@
+package tests.day08_testbase_dropdown;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBase_Each;
+
+public class C02_DropdownMenu extends TestBase_Each {
+
+    // ● https://testotomasyonu.com/form adresine gidin.
+    //1.Dogum tarihi gun seçeneğinden index kullanarak 5’i secin
+    //2. Dogum tarihi ay seçeneğinden value kullanarak Nisan’i secin
+    //3. Dogum tarihi yil seçeneğinden visible text kullanarak 1990’i secin
+    //4. Secilen değerleri konsolda yazdirin
+    //5. Ay dropdown menüdeki tum değerleri(value) yazdırın
+    //6. Ay Dropdown menusunun boyutunun 13 olduğunu test edin
+
+    @Test
+    public void dropdownTesti(){
+
+        // ● https://testotomasyonu.com/form adresine gidin.
+        //1.Dogum tarihi gun seçeneğinden index kullanarak 5’i secin
+        //2. Dogum tarihi ay seçeneğinden value kullanarak Nisan’i secin
+        //3. Dogum tarihi yil seçeneğinden visible text kullanarak 1990’i secin
+        //4. Secilen değerleri konsolda yazdirin
+        //5. Ay dropdown menüdeki tum değerleri(value) yazdırın
+        //6. Ay Dropdown menusunun boyutunun 13 olduğunu test edin
+
+    }
+}
