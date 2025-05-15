98baad7fd88577742abfe518e8c880b30fa748ee
bulutluoz <abbulutluoz@gmail.com>
2025-05-15 20:42:09 +0200

    8.gun sonu

--- src/test/java/tests/day08_testbase_dropdown/C02_DropdownMenu.java
+++ src/test/java/tests/day08_testbase_dropdown/C02_DropdownMenu.java
@@ -1,7 +1,15 @@
 package tests.day08_testbase_dropdown;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utilities.ReusableMethods;
 import utilities.TestBase_Each;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class C02_DropdownMenu extends TestBase_Each {
 
@@ -17,12 +25,67 @@ public class C02_DropdownMenu extends TestBase_Each {
     public void dropdownTesti(){
 
         // ● https://testotomasyonu.com/form adresine gidin.
+        driver.get("https://testotomasyonu.com/form");
         //1.Dogum tarihi gun seçeneğinden index kullanarak 5’i secin
+            // 1.adim : dropdown webelemntini locate edip kaydedelim
+        WebElement gunDropdown = driver.findElement(By.xpath("(//select[@class='form-control'])[1]"));
+            // 2.adim : bir select objesi olusturalim, parametre olarak dropdown elemntini yazalim
+        Select selectDay = new Select(gunDropdown);
+            // 3.adim : select objesi ile istenen islemi yapin
+        selectDay.selectByIndex(5);
+
+
         //2. Dogum tarihi ay seçeneğinden value kullanarak Nisan’i secin
+            // 1.adim
+        WebElement ayDropdown = driver.findElement(By.xpath("(//select[@class='form-control'])[2]"));
+            // 2.adim
+        Select selectAy = new Select(ayDropdown);
+            // 3.adim
+        selectAy.selectByValue("nisan");
+
+
         //3. Dogum tarihi yil seçeneğinden visible text kullanarak 1990’i secin
+            // 1.adim
+        WebElement yilDropdown = driver.findElement(By.xpath("(//select[@class='form-control'])[3]"));
+            // 2.adim
+        Select selectYil = new Select(yilDropdown);
+            // 3.adim
+        selectYil.selectByVisibleText("1990");
+
+
         //4. Secilen değerleri konsolda yazdirin
+        System.out.println(selectDay.getFirstSelectedOption().getText());
+        System.out.println(selectAy.getFirstSelectedOption().getText());
+        System.out.println(selectYil.getFirstSelectedOption().getText());
+
+
         //5. Ay dropdown menüdeki tum değerleri(value) yazdırın
+        List<WebElement> ayMenusuTumOptionElementleri = selectAy.getOptions();
+
+        // WebElement'lerden olusan bir listeyi direkt yazdiramayiz
+        // for loop ile her bir elementi ele alip
+        // webelemnt uzerindeki yaziyi, olusturacagimiz bir String Listeye ekleyelim
+
+
+        List<String> optionMetinleriList = new ArrayList<>();
+
+        for (WebElement eachElement :ayMenusuTumOptionElementleri){
+            optionMetinleriList.add(eachElement.getText());
+        }
+
+        System.out.println(optionMetinleriList);
+        // [Ay, Ocak, Şubat, Mart, Nisan, Mayıs, Haziran, Temmuz, Ağustos, Eylül, Ekim, Kasım, Aralık]
+
+
         //6. Ay Dropdown menusunun boyutunun 13 olduğunu test edin
+
+        int expectedMenuBoyutu = 13;
+        int actualMenuBoyutu = ayMenusuTumOptionElementleri.size();
+
+
+        Assertions.assertEquals(expectedMenuBoyutu,actualMenuBoyutu);
+
+        ReusableMethods.bekle(3);
 
     }
 }
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -2,4 +2,21 @@ package utilities;
 
 public class ReusableMethods {
 
+    public static void bekle(int saniye)  {
+
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    // verilen Webelement'lerden olusan bir listedeki
+    // herbir webelement'i ele alip
+    // web element uzerindeki yaziyi, olusturacagimiz bir listeye ekleyip,
+    // tum elementler elden gectikten sonra
+    // String'lerden olusan listeyi DONDUREN bir method olusturun
+
+
 }
