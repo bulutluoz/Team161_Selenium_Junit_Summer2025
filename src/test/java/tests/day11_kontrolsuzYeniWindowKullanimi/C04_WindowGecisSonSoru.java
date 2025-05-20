57baf4e9c668a035e0f61ada2f2d12b78562d8f6
bulutluoz <abbulutluoz@gmail.com>
2025-05-20 21:10:01 +0200

    11.gun sonu

---
+++ src/test/java/tests/day11_kontrolsuzYeniWindowKullanimi/C04_WindowGecisSonSoru.java
@@ -0,0 +1,34 @@
+package tests.day11_kontrolsuzYeniWindowKullanimi;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBase_Each;
+
+public class C04_WindowGecisSonSoru extends TestBase_Each {
+    @Test
+    public void test01(){
+
+        // testotomasyonu anasayfaya gidin
+
+        // yeni bir tab acarak, wisequarter.com.tr adresine gidin
+
+        // bagimsiz yeni bir window acarak youtube sayfasina gidin
+
+        // Reusable methods classindaki window degistirme method'unu kullanarak
+
+        // testotomasyonu'nun acik oldugu window'a donun
+
+        // url'in testotomasyonu icerdigini test edin
+
+        // wise quarter'inn acik oldugu window'a donun
+
+        // title'in Wise icerdigini test edin
+
+        // youtube'un acik oldugu window'a donun
+
+        // url'in youtube icerdigini test edin
+
+
+
+
+    }
+}
