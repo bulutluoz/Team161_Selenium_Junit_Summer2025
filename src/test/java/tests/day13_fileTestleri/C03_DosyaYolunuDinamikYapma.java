a1a384e4d21e629212c6c7a0d86f485f2153e2e6
bulutluoz <abbulutluoz@gmail.com>
2025-05-22 20:28:45 +0200

    13.gun sonu

--- src/test/java/tests/day13_fileTestleri/C03_DosyaYolunuDinamikYapma.java
+++ src/test/java/tests/day13_fileTestleri/C03_DosyaYolunuDinamikYapma.java
@@ -25,7 +25,38 @@ public class C03_DosyaYolunuDinamikYapma {
         // herkesin en sondaki  "Desktop/selenium.txt" kismi ORTAK
         // Desktop'a kadar olan kisim herkeste FARKLI
 
+        // String dosyaYolu = herkesteFarkliOlanKisim       + herkesteAyniOlanKisim
+
+        //                    /Users/ahmetbulutluoz/        +  Desktop/selenium.txt
+        //                    C:\\Users\\casper\\           +  Desktop\\Selenium.txt
+        //                    C:\\Users\\RIDVAN\\           +  Desktop\\selenium.txt
+
+        // Java herkeste farkli olan BASTAKI KISIM icin bize
+        // dinamik bir kod hazirlamis
+
+        System.out.println(System.getProperty("user.home"));
+        // /Users/ahmetbulutluoz
+        // butun bilgisayarlar icin home dosya yolunu verir
+        // Download, Desktop .... gibi klasorler
+        // bu home dosya yolundan sonra gelir
 
         Assertions.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+
+
+        //                          herkesteFarkliOlanKisim       + herkesteAyniOlanKisim
+        String dinamikDosyaYolu =  System.getProperty("user.home") + "/Desktop/selenium.txt";
+
+        Assertions.assertTrue(Files.exists(Paths.get(dinamikDosyaYolu)));
+
+
+
+
+
+
+
+
+
+
     }
 }
