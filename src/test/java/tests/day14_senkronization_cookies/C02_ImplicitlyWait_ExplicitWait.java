40f723086f43a0f29cd4934a0565550c3b34c3d3
bulutluoz <abbulutluoz@gmail.com>
2025-05-23 20:31:26 +0200

    14.gun sonu

--- src/test/java/tests/day14_senkronization_cookies/C02_ImplicitlyWait_ExplicitWait.java
+++ src/test/java/tests/day14_senkronization_cookies/C02_ImplicitlyWait_ExplicitWait.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -35,6 +36,16 @@ public class C02_ImplicitlyWait_ExplicitWait {
                 .click();
 
         //3. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+        /*
+            remove butonuna basar basmaz
+            itsGoneElementi'ni locate ediyoruz
+
+            Aslinda itsGoneElementi 3 saniye sonra geliyor
+            ama olmayan bir webelement'in locate edilebilmesi icin bekleme gorevi
+            implicitly wait'in gorev alanina girdigi icin
+            implicitly wait bekleme islemini hallediyor
+
+         */
         WebElement itsGoneElementi = driver.findElement(By.xpath("//*[.=\"It's gone!\"]"));
 
         Assertions.assertTrue(itsGoneElementi.isDisplayed());
@@ -58,14 +69,37 @@ public class C02_ImplicitlyWait_ExplicitWait {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        // explicit wait, implicitlyWait'in yeterli olmadigi durumlarda kullanilir
+        // implicitly wait'in bekleme suresini bilerek azalttik
 
 
         //1. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
         //2. Remove butonuna basin.
+        driver.findElement(By.xpath("//*[@onclick='swapCheckbox()']"))
+                .click();
+
         //3. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+
+        // explicit wait icin 3 adima ihtiyac var
+        // 1.adim : webdriver wait objesi olusturmak
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+
+        // 2.adim : kullanilacak webelement'i locate edip kaydedelim
+        WebElement itsGoneElementi = driver.findElement(By.xpath("//*[.=\"It's gone!\"]"));
+
+        // 3.adim : spesifik webelement ve spesifik gorev icin wait objesi ile bekleme yapin
+
+
+
+
+
         //4. Add buttonuna basin
         //5. It’s back mesajinin gorundugunu test edin
+
+        driver.quit();
 
     }
 
