e51d2099173eb3c26aec664607ff71202fb197c7
bulutluoz <abbulutluoz@gmail.com>
2025-05-29 19:44:35 +0200

    18.gun sonu

--- src/test/java/tests/day18_sceenshot_jsExecutor/C03_SpesifikWebelementScreenshot.java
+++ src/test/java/tests/day18_sceenshot_jsExecutor/C03_SpesifikWebelementScreenshot.java
@@ -1,16 +1,22 @@
 package tests.day18_sceenshot_jsExecutor;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
 import utilities.TestBase_Each;
+
+import java.io.File;
+import java.io.IOException;
 
 public class C03_SpesifikWebelementScreenshot extends TestBase_Each {
 
     @Test
-    public void test01(){
+    public void test01() throws IOException {
 
         // Testotomasyonu anasayfaya gidin
         driver.get("https://www.testotomasyonu.com");
@@ -26,8 +32,24 @@ public class C03_SpesifikWebelementScreenshot extends TestBase_Each {
         String actualAramaSonucu = aramaSonucElementi.getText();
 
         Assertions.assertNotEquals(unExpectedAramaSonucu,actualAramaSonucu);
+        ReusableMethods.bekle(1);
 
         // arama sonucu elementinin screenshot'ini alip kaydedin
+
+        // 1.adim : screenshot alacaginiz webelementi locate edip, kaydedin
+        //          biz arama sonuc yazisi elementini zaten locate edip kaydetmistik
+
+        // 2.adim : screenshot'i kaydedecegimiz dosyayi(File) olusturun
+        String dosyaYolu = "target/screenshots/istenenWebElement.jpg";
+        File asilResim = new File(dosyaYolu);
+
+        // 3.adim : webElement'i kullanarak screenshot alip
+        //          gecici bir dosyaya(File) kaydedin
+        File geciciResim = aramaSonucElementi.getScreenshotAs(OutputType.FILE);
+
+        // 4.adim : gecici resmi asil resme kopyalayin
+        FileUtils.copyFile(geciciResim,asilResim);
+
 
         // ilk urunu tiklayin
 
